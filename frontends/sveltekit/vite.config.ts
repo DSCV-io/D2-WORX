917425d441815ee3016296caf99605264de5dcd0
Tr-st-n <91633584+Tr-st-n@users.noreply.github.com>
2025-10-02 14:24:15 -0600

    feat(frontend): scaffolded out sveltekit project

---
+++ frontends/sveltekit/vite.config.ts
@@ -0,0 +1,62 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd());
+  const allowedHosts = safeParseArray(env.VITE_ALLOWED_HOSTS || '');
+
+	return {
+    server: {
+      allowedHosts: allowedHosts,
+    },
+    plugins: [
+      tailwindcss(),
+      sveltekit(),
+      paraglideVitePlugin({
+        project: './project.inlang',
+        outdir: './src/lib/paraglide'
+      })
+    ],
+    test: {
+      expect: { requireAssertions: true },
+      projects: [
+        {
+          extends: './vite.config.ts',
+          test: {
+            name: 'client',
+            environment: 'browser',
+            browser: {
+              enabled: true,
+              provider: 'playwright',
+              instances: [{ browser: 'chromium' }]
+            },
+            include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+            exclude: ['src/lib/server/**'],
+            setupFiles: ['./vitest-setup-client.ts']
+          }
+        },
+        {
+          extends: './vite.config.ts',
+          test: {
+            name: 'server',
+            environment: 'node',
+            include: ['src/**/*.{test,spec}.{js,ts}'],
+            exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+          }
+        }
+      ]
+    }
+  }
+});
+
+function safeParseArray(v: string): string[] {
+  try {
+    const split = v.split(',');
+    return split.map(item => item.trim());
+  } catch {
+    return [];
+  }
+}
